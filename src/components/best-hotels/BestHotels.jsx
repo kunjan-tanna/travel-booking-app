e0b7410c8e2e3eb46a5d0298749d06c60a1ad236
kunjan_tanna <kvtanna.om@gmail.com>
2024-10-31 16:27:11 +0530

    Implement the Design for best hotels components

--- src/components/best-hotels/BestHotels.jsx
+++ src/components/best-hotels/BestHotels.jsx
@@ -1,7 +1,63 @@
 import React from "react";
+import image_1 from "../../../public/assets/hr_1.jpg";
+import image_2 from "../../../public/assets/hr_2.jpg";
+import image_3 from "../../../public/assets/hr_3.jpg";
+import image_4 from "../../../public/assets/hr_4.jpg";
+import Card from "./Card";
 
 function BestHotels() {
-  return <div>BestHotels</div>;
+  const data = [
+    {
+      name: "Arbian Paradise",
+      image: image_1,
+      price: 324.5,
+      category: "Luxury",
+      reviews: 4.7,
+      location: "Dubai, UAE",
+    },
+    {
+      name: "Arbian Paradise",
+      image: image_2,
+      price: 324.5,
+      category: "Luxury",
+      reviews: 4.7,
+      location: "Dubai, UAE",
+    },
+    {
+      name: "Arbian Paradise",
+      image: image_3,
+      price: 324.5,
+      category: "Luxury",
+      reviews: 4.7,
+      location: "Dubai, UAE",
+    },
+    {
+      name: "Arbian Paradise",
+      image: image_4,
+      price: 324.5,
+      category: "Luxury",
+      reviews: 4.7,
+      location: "Dubai, UAE",
+    },
+  ];
+  return (
+    <div className="h-full w-full my-36">
+      <div className="h-full w-5/6 mx-auto flex flex-col justify-start">
+        <h5 className="text-[20px] bg-blue-500 text-white rounded-full p-4 w-max">
+          {" "}
+          Explore Top
+        </h5>
+        <h2 className="text-4xl text-slate-800 font-bold mt-6 mb-12">
+          Best Hotels
+        </h2>
+        <div className="flex flex-wrap items-center gap-2">
+          {data?.map((place, index) => (
+            <Card key={index} place={place} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default BestHotels;
---
+++ src/components/best-hotels/Card.jsx
@@ -0,0 +1,48 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { AiFillStar } from "react-icons/ai";
+import { format } from "currency-formatter";
+const Card = ({ place }) => {
+  return (
+    <Link
+      href={"/details/1"}
+      className="cursor-pointer h-[500px] w-[350px] flex flex-wrap rounded-xl shadow-md transition-all"
+    >
+      <div className="relative h-2/3 w-full">
+        <Image
+          src={place.image}
+          className="h-full w-full overflow-hidden rounded-tl-xl rounded-tr-xl object-cover"
+          alt="LocationImage"
+        />
+        <div className="absolute right-0 bottom-0 p-4 bg-blue-700 text-white rounded-tl-xl font-semibold">
+          {place.location}
+        </div>
+      </div>
+      {/* DATA */}
+      <div className="w-full flex flex-col gap-4 p-4">
+        <div className="mt-2 flex justify-between items-center">
+          <h2 className="text-left text-2xl text-slate-800 font-semibold">
+            {place.name}
+          </h2>
+          <span className="p-2 rounded-full bg-blue-600 text-white flex items-center gap-2">
+            <AiFillStar color="white" />
+            <span className="text-white">{place.reviews}</span>
+          </span>
+        </div>
+        {/* Price & Reviews  */}
+        <div className="mt-6 flex justify-between items-center">
+          <span className="text-slate-600">
+            {format(place.price, { locale: "en-US" })}
+            <span className="ml-2">per night</span>
+          </span>
+          <button className="cursor-pointer py-2 px-6 text-white bg-blue-500 ">
+            Book
+          </button>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default Card;
