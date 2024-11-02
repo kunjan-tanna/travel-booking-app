27811f63cf7a77c6adca3fbc8fb32c304494ae63
kunjan_tanna <kvtanna.om@gmail.com>
2024-11-02 17:33:45 +0530

    Implemented the signUp and login UI page

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -1,31 +1,38 @@
 "use client";
-import Image from "next/image";
-import Paris from "../../../public/assets/paris.jpg";
-import React from "react";
-import Input from "@/ui/Input";
 import Button from "@/ui/Button";
+import Input from "@/ui/Input";
+import Image from "next/image";
+import Dubai from "../../../public/assets/dubai.jpg";
+import React from "react";
 
-const Login = () => {
+const SignUp = () => {
   return (
     <div className="relative h-screen w-full">
       <div className="relative h-full w-full">
         <Image
+          src={Dubai}
           className="brightness-50 h-full w-full object-cover"
-          src={Paris}
-          alt="ParisImage"
+          alt="DubaiImage"
         />
-        <div className="h-[350px] w-[350px] bg-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg">
-          <h2 className="text-center p-4 font-semibold] text-slate-800 text-2xl border-b border-slate-500">
-            Log into your account
+        <div className="h-[450px] w-[400px] bg-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg">
+          <h2 className="text-center p-4 font-semibold text-slate-800 text-2xl border-b border-slate-500">
+            Create an account
           </h2>
-          <form className="mt-8 flex flex-col w-full gap-8">
+          <form className="mt-12 flex flex-col  items-center w-full gap-8">
+            <Input
+              className={
+                "w-full mx-auto outline-none border border-slate-400 py-1 px-3 rounded-md focus:border-slate-600"
+              }
+              type={"text"}
+              placeholder={"John123"}
+            />
             <Input
               className={
                 "w-full mx-auto outline-none border border-slate-400 py-1 px-3 rounded-md focus:border-slate-600"
               }
               type={"email"}
-              placeholder={"john@gmail.com"}
-            />
+              placeholder={"John@gmail.com"}
+            />{" "}
             <Input
               className={
                 "w-full mx-auto outline-none border border-slate-400 py-1 px-3 rounded-md focus:border-slate-600"
@@ -46,4 +53,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default SignUp;
