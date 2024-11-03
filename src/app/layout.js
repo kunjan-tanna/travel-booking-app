c6d09a65d5e33d6a1814f705043dd13c30318084
kunjan_tanna <kvtanna.om@gmail.com>
2024-11-03 12:21:10 +0530

    Prisma and monogoDB setup is done

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import LayoutProvider from "@/components/layout-provider/LayoutProvider";
+import Toast from "@/utils/toast";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,6 +25,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Toast />
         <LayoutProvider>{children}</LayoutProvider>
       </body>
     </html>
---
+++ src/utils/axiosAPI.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const baseURL = "http://localhost:3000/api/";
+
+const AXIOS_API = axios.create({
+  baseURL,
+});
+export default AXIOS_API;
